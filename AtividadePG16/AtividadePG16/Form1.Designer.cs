85a49d0ab43bd38a30121d07d05b83cebdbe7ced
Victor Alecrim <94874313+Victoralecrim@users.noreply.github.com>
2023-03-27 17:31:58 -0300

    Add files via upload

---
+++ AtividadePG16/AtividadePG16/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace AtividadePG16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.rdbSim1 = new System.Windows.Forms.RadioButton();
+            this.rdbNao1 = new System.Windows.Forms.RadioButton();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.rdbSim3 = new System.Windows.Forms.RadioButton();
+            this.rdbNao3 = new System.Windows.Forms.RadioButton();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.rdbSim5 = new System.Windows.Forms.RadioButton();
+            this.rdbNao5 = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.rdbSim2 = new System.Windows.Forms.RadioButton();
+            this.rdbNao2 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rdbSim4 = new System.Windows.Forms.RadioButton();
+            this.rdbNao4 = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel5.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.rdbSim1);
+            this.panel5.Controls.Add(this.rdbNao1);
+            this.panel5.Location = new System.Drawing.Point(178, 118);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(133, 31);
+            this.panel5.TabIndex = 29;
+            // 
+            // rdbSim1
+            // 
+            this.rdbSim1.AutoSize = true;
+            this.rdbSim1.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.rdbSim1.Location = new System.Drawing.Point(3, 2);
+            this.rdbSim1.Name = "rdbSim1";
+            this.rdbSim1.Size = new System.Drawing.Size(55, 22);
+            this.rdbSim1.TabIndex = 0;
+            this.rdbSim1.TabStop = true;
+            this.rdbSim1.Text = "Sim";
+            this.rdbSim1.UseVisualStyleBackColor = true;
+            // 
+            // rdbNao1
+            // 
+            this.rdbNao1.AutoSize = true;
+            this.rdbNao1.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.rdbNao1.Location = new System.Drawing.Point(66, 2);
+            this.rdbNao1.Name = "rdbNao1";
+            this.rdbNao1.Size = new System.Drawing.Size(57, 22);
+            this.rdbNao1.TabIndex = 14;
+            this.rdbNao1.TabStop = true;
+            this.rdbNao1.Text = "Não";
+            this.rdbNao1.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.rdbSim3);
+            this.panel3.Controls.Add(this.rdbNao3);
+            this.panel3.Location = new System.Drawing.Point(178, 312);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(133, 31);
+            this.panel3.TabIndex = 27;
+            // 
+            // rdbSim3
+            // 
+            this.rdbSim3.AutoSize = true;
+            this.rdbSim3.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.rdbSim3.Location = new System.Drawing.Point(3, 3);
+            this.rdbSim3.Name = "rdbSim3";
+            this.rdbSim3.Size = new System.Drawing.Size(55, 22);
+            this.rdbSim3.TabIndex = 0;
+            this.rdbSim3.TabStop = true;
+            this.rdbSim3.Text = "Sim";
+            this.rdbSim3.UseVisualStyleBackColor = true;
+            // 
+            // rdbNao3
+            // 
+            this.rdbNao3.AutoSize = true;
+            this.rdbNao3.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.rdbNao3.Location = new System.Drawing.Point(73, 3);
+            this.rdbNao3.Name = "rdbNao3";
+            this.rdbNao3.Size = new System.Drawing.Size(57, 22);
+            this.rdbNao3.TabIndex = 14;
+            this.rdbNao3.TabStop = true;
+            this.rdbNao3.Text = "Não";
+            this.rdbNao3.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.rdbSim5);
+            this.panel4.Controls.Add(this.rdbNao5);
+            this.panel4.Location = new System.Drawing.Point(719, 243);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(139, 31);
+            this.panel4.TabIndex = 28;
+            // 
+            // rdbSim5
+            // 
+            this.rdbSim5.AutoSize = true;
+            this.rdbSim5.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.rdbSim5.Location = new System.Drawing.Point(3, 3);
+            this.rdbSim5.Name = "rdbSim5";
+            this.rdbSim5.Size = new System.Drawing.Size(55, 22);
+            this.rdbSim5.TabIndex = 0;
+            this.rdbSim5.TabStop = true;
+            this.rdbSim5.Text = "Sim";
+            this.rdbSim5.UseVisualStyleBackColor = true;
+            // 
+            // rdbNao5
+            // 
+            this.rdbNao5.AutoSize = true;
+            this.rdbNao5.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.rdbNao5.Location = new System.Drawing.Point(79, 3);
+            this.rdbNao5.Name = "rdbNao5";
+            this.rdbNao5.Size = new System.Drawing.Size(57, 22);
+            this.rdbNao5.TabIndex = 14;
+            this.rdbNao5.TabStop = true;
+            this.rdbNao5.Text = "Não";
+            this.rdbNao5.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.rdbSim2);
+            this.panel2.Controls.Add(this.rdbNao2);
+            this.panel2.Location = new System.Drawing.Point(181, 211);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(130, 31);
+            this.panel2.TabIndex = 26;
+            // 
+            // rdbSim2
+            // 
+            this.rdbSim2.AutoSize = true;
+            this.rdbSim2.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.rdbSim2.Location = new System.Drawing.Point(3, 3);
+            this.rdbSim2.Name = "rdbSim2";
+            this.rdbSim2.Size = new System.Drawing.Size(55, 22);
+            this.rdbSim2.TabIndex = 0;
+            this.rdbSim2.TabStop = true;
+            this.rdbSim2.Text = "Sim";
+            this.rdbSim2.UseVisualStyleBackColor = true;
+            // 
+            // rdbNao2
+            // 
+            this.rdbNao2.AutoSize = true;
+            this.rdbNao2.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.rdbNao2.Location = new System.Drawing.Point(70, 3);
+            this.rdbNao2.Name = "rdbNao2";
+            this.rdbNao2.Size = new System.Drawing.Size(57, 22);
+            this.rdbNao2.TabIndex = 14;
+            this.rdbNao2.TabStop = true;
+            this.rdbNao2.Text = "Não";
+            this.rdbNao2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rdbSim4);
+            this.panel1.Controls.Add(this.rdbNao4);
+            this.panel1.Location = new System.Drawing.Point(719, 131);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(139, 33);
+            this.panel1.TabIndex = 25;
+            // 
+            // rdbSim4
+            // 
+            this.rdbSim4.AutoSize = true;
+            this.rdbSim4.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.rdbSim4.Location = new System.Drawing.Point(3, 4);
+            this.rdbSim4.Name = "rdbSim4";
+            this.rdbSim4.Size = new System.Drawing.Size(55, 22);
+            this.rdbSim4.TabIndex = 0;
+            this.rdbSim4.TabStop = true;
+            this.rdbSim4.Text = "Sim";
+            this.rdbSim4.UseVisualStyleBackColor = true;
+            // 
+            // rdbNao4
+            // 
+            this.rdbNao4.AutoSize = true;
+            this.rdbNao4.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.rdbNao4.Location = new System.Drawing.Point(75, 4);
+            this.rdbNao4.Name = "rdbNao4";
+            this.rdbNao4.Size = new System.Drawing.Size(57, 22);
+            this.rdbNao4.TabIndex = 14;
+            this.rdbNao4.TabStop = true;
+            this.rdbNao4.Text = "Não";
+            this.rdbNao4.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(663, 194);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(233, 20);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Você já trabalhou com a vitima?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(622, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(336, 20);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Você devia algum valor ou coisa para a vitima?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(137, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(202, 20);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Você mora perto da vitima?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(130, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(228, 20);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Você esteve no local do crime?";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(531, 359);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(21, 22);
+            this.lblResultado.TabIndex = 20;
+            this.lblResultado.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(91, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(322, 20);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Você telefonou para a vitima recentemente?";
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnEnviar.Location = new System.Drawing.Point(471, 302);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(146, 41);
+            this.btnEnviar.TabIndex = 18;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(467, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Exercicio 10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1120, 450);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.RadioButton rdbSim1;
+        private System.Windows.Forms.RadioButton rdbNao1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.RadioButton rdbSim3;
+        private System.Windows.Forms.RadioButton rdbNao3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.RadioButton rdbSim5;
+        private System.Windows.Forms.RadioButton rdbNao5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton rdbSim2;
+        private System.Windows.Forms.RadioButton rdbNao2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rdbSim4;
+        private System.Windows.Forms.RadioButton rdbNao4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AtividadePG16/AtividadePG16/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtividadePG16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            int Valor = 0;
+
+            if (rdbSim1.Checked == true)
+            {
+                Valor++;
+            }
+            else
+            {
+                lblResultado.Text = "Pressione todos os botoes";
+            }
+
+            if (rdbSim2.Checked == true)
+            {
+                Valor++;
+            }
+
+            else
+            {
+                lblResultado.Text = "Pressione todos os botoes";
+            }
+
+            if (rdbSim3.Checked == true)
+            {
+                Valor++;
+            }
+            else
+            {
+                lblResultado.Text = "Pressione todos os botoes";
+            }
+
+            if (rdbSim4.Checked == true)
+            {
+                Valor++;
+            }
+            else
+            {
+                lblResultado.Text = "Pressione todos os botoes";
+            }
+            if (rdbSim5.Checked == true)
+            {
+                Valor++;
+            }
+            else
+            {
+                lblResultado.Text = "Pressione todos os botoes";
+            }
+
+            switch (Valor)
+            {
+                case 2:
+                    lblResultado.Text = "Ele é suspeito";
+                    break;
+                case 3:
+                case 4:
+                    lblResultado.Text = "Ele é cumplice";
+                    break;
+                case 5:
+                    lblResultado.Text = "Ele é o assasino";
+                    break;
+                default:
+                    lblResultado.Text = "Ele é inocente";
+                    break;
+            }
+        }
+    }
+
+}
