ae93533ff5444824bd1a8c3cccb7a79eedff9320
Victor Alecrim <94874313+Victoralecrim@users.noreply.github.com>
2023-03-27 17:31:26 -0300

    Add files via upload

---
+++ ExerciciosPAG18/ExerciciosPAG18/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace ExerciciosPAG18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblResultado3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnCalcular3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite o numero do mês: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 154);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(92, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Exibir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Exercicio 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(410, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Exercicio 2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(312, 154);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(309, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "1-Importado -- 2-Nacional";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(467, 154);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(464, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(129, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Digite o valor do produto: ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(413, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Exibir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtMes
+            // 
+            this.txtMes.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtMes.Location = new System.Drawing.Point(713, 155);
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(251, 20);
+            this.txtMes.TabIndex = 25;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label8.Location = new System.Drawing.Point(767, 115);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(149, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Escreva um número de 1 a 12";
+            // 
+            // lblResultado3
+            // 
+            this.lblResultado3.AutoSize = true;
+            this.lblResultado3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado3.Location = new System.Drawing.Point(837, 251);
+            this.lblResultado3.Name = "lblResultado3";
+            this.lblResultado3.Size = new System.Drawing.Size(21, 22);
+            this.lblResultado3.TabIndex = 23;
+            this.lblResultado3.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label6.Location = new System.Drawing.Point(814, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Exercício 3";
+            // 
+            // btnCalcular3
+            // 
+            this.btnCalcular3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btnCalcular3.Location = new System.Drawing.Point(801, 201);
+            this.btnCalcular3.Name = "btnCalcular3";
+            this.btnCalcular3.Size = new System.Drawing.Size(85, 28);
+            this.btnCalcular3.TabIndex = 21;
+            this.btnCalcular3.Text = "Calcular";
+            this.btnCalcular3.UseVisualStyleBackColor = true;
+            this.btnCalcular3.Click += new System.EventHandler(this.btnCalcular3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 450);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblResultado3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnCalcular3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblResultado3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnCalcular3;
+    }
+}
+
---
+++ ExerciciosPAG18/ExerciciosPAG18/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExerciciosPAG18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numero;
+
+            numero = int.Parse(textBox1.Text);
+
+            switch (numero)
+            {
+                case 1:
+                    MessageBox.Show("Janeiro");
+                    break;
+                case 2:
+                    MessageBox.Show("Fevereiro");
+                    break;
+                case 3:
+                    MessageBox.Show("Março");
+                    break;
+                case 4:
+                    MessageBox.Show("Abril");
+                    break;
+                case 5:
+                    MessageBox.Show("Maio");
+                    break;
+                case 6:
+                    MessageBox.Show("Junho");
+                    break;
+                case 7:
+                    MessageBox.Show("Julho");
+                    break;
+                case 8:
+                    MessageBox.Show("Agosto");
+                    break;
+                case 9:
+                    MessageBox.Show("Setembro");
+                    break;
+                case 10:
+                    MessageBox.Show("Outubro");
+                    break;
+                case 11:
+                    MessageBox.Show("Novembro");
+                    break;
+                case 12:
+                    MessageBox.Show("Dezembro");
+                    break;
+                default:
+                    MessageBox.Show("Não existe mês correspondente a esse numero");
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int escolha;
+            double valorProd;
+
+            escolha = int.Parse(textBox2.Text);
+            valorProd = int.Parse(textBox3.Text);
+
+            if (escolha == 1)
+            {
+                valorProd = int.Parse(textBox3.Text) * 5.27;
+                MessageBox.Show($"O produto é importado, seu valor é {valorProd}");
+            }
+            else if (escolha == 2)
+            {
+                MessageBox.Show("O produto é nacional e custa: " + textBox3.Text);
+            }
+        }
+
+        private void btnCalcular3_Click(object sender, EventArgs e)
+        {
+            lblResultado3.MaximumSize = new Size(250, 0);
+            lblResultado3.AutoSize = true;
+            int mes = int.Parse(txtMes.Text);
+            string dias = " ";
+
+            switch (mes)
+            {
+                case 1:
+                    dias = " com 31 dias";
+                    lblResultado3.Text = "Janeiro" + dias;
+                    break;
+                case 2:
+                    dias = " com 28 ou 29 dias";
+                    lblResultado3.Text = "Fevereiro" + dias;
+                    break;
+                case 3:
+                    dias = " com 31 dias";
+                    lblResultado3.Text = "Março" + dias;
+                    break;
+                case 4:
+                    dias = " com 30 dias";
+                    lblResultado3.Text = "Abril" + dias;
+                    break;
+                case 5:
+                    dias = " com 31 dias";
+                    lblResultado3.Text = "Maio" + dias;
+                    break;
+                case 6:
+                    dias = " com 30 dias";
+                    lblResultado3.Text = "Junho" + dias;
+                    break;
+                case 7:
+                    dias = " com 31 dias";
+                    lblResultado3.Text = "Julho" + dias;
+                    break;
+                case 8:
+                    dias = " com 31 dias";
+                    lblResultado3.Text ="Agosto" + dias;
+                    break;
+                case 9:
+                    dias = " com 30 dias";
+                    lblResultado3.Text = "Setembro" + dias;
+                    break;
+                case 10:
+                    dias = " com 31 dias";
+                    lblResultado3.Text = "Outubro" + dias;
+                    break;
+                case 11:
+                    dias = " com 30 dias";
+                    lblResultado3.Text = "Novembro" + dias;
+                    break;
+                case 12:
+                    dias = " com 31 dias";
+                    lblResultado3.Text = "Dezembro" + dias;
+                    break;
+
+                default:
+                    lblResultado3.Text = "Número Inválido";
+                    break;
+            }
+        }
+    }
+}
